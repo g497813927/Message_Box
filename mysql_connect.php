560ea582fd2d3ecf43724e7053a721ae78417c12
赵佳成 <admin@g497813927.com>
2019-08-08 20:57:11 +0800

    Update mysql_connect.php

--- mysql_connect.php
+++ mysql_connect.php
@@ -6,7 +6,7 @@
 	*/
 	$servername = "localhost";  //If you are not use sql locally, pls add your sql location
 	$sqlusername = "";          //Add your SQL username here
-	$sqlpwd = "CSP_create_task";//Add your SQL password
+	$sqlpwd = "";		    //Add your SQL password
 	$connect = mysqli_connect($servername, $sqlusername, $sqlpwd);
 	if (!$connect) {
     	die("Connection failed: " . mysqli_connect_error());
